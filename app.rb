39d226412c07a9a3ed34c7634c27ef20f760c1af
Xiaowen Xu <604233023@qq.com>
2018-10-16 21:21:51 -0400

    appp update

--- app.rb
+++ app.rb
@@ -83,6 +83,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
     puts slots.to_s
 
     cityname = slots["city"]["value"]
+    cityname = slots["city"].to_json
 
     #cityname = request.intent.slots.city.["name"]
 
