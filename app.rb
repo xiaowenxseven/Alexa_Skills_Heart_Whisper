eed86f270e9edab616b8733aa2f2a54ec95af6f8
Xiaowen Xu <604233023@qq.com>
2018-10-16 21:04:24 -0400

    sm

--- app.rb
+++ app.rb
@@ -84,7 +84,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
 
     #cityname = request.intent.slots.city.["name"]
 
-    response.set_output_speech_text("If you see this #{ cityname }. congratulations!")
+    response.set_output_speech_text("If you see this #{cityname}. congratulations!")
     response.set_simple_card("Soul Wanderer", "City test is processed")
     logger.info 'City test processed'
   end
